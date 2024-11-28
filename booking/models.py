1c83f9cf411145c7b1bf81d252e6ce7ac6bb00c6
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-11-28 02:38:47 +0000

    added Reservation and logic to createing the reservation

--- booking/models.py
+++ booking/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    reservation_date = models.DateTimeField()
+    number_of_guests = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} - {self.reservation_date}"
