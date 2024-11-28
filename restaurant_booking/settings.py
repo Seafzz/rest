35c87363376032f06f3991a27eab6fa140d9721e
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-11-28 05:10:05 +0000

    update: updated where to django should look for static files

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -92,5 +92,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [ 
+    os.path.join(BASE_DIR, 'booking/static'), 
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
