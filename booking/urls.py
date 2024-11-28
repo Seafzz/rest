3e632b52589733dc0463c0ce425604360d6ffcbd
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-11-28 04:39:11 +0000

    added edit and dete reservation urlpaths

--- booking/urls.py
+++ booking/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('reservations/', views.reservation_list, name='reservation_list'),
     path('create/', views.create_reservation, name='create_reservation'),
+    path('edit/<int:pk>/', views.edit_reservation, name='edit_reservation'),
+    path('delete/<int:pk>/', views.delete_reservation, name='delete_reservation'),
 ]
+
