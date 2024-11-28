343ddeefb61e71621c2070cfe978c2654623d5ee
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-11-28 02:40:42 +0000

    added all the imports I need for this project

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect 
+from django.contrib.auth import login, logout, authenticate 
+from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth.decorators import login_required 
+from .models import Reservation 
+from .forms import ReservationForm
