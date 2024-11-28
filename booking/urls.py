c0130077464ad14a19b38f73d7fb47d465fc0222
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-11-28 03:03:51 +0000

    added url paths to all the hthmls

--- booking/urls.py
+++ booking/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('reservations/', views.reservation_list, name='reservation_list'),
+    path('create/', views.create_reservation, name='create_reservation'),
 ]
