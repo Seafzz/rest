f6384cf7ff61693fa8161a68186fd39038f18b7b
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-03 23:51:29 +0000

    created signals.py

--- booking/models.py
+++ booking/models.py
@@ -11,9 +11,10 @@ class Reservation(models.Model):
         return self.name
 
 
-class UserProfile(models.model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.Charfield(max_length15, blank=True, null=True)
-    addresss = models.TextField(blank=True, null=True)
-
-    def __str__(self): return self
+class UserProfile(models.Model): 
+    user = models.OneToOneField(User, on_delete=models.CASCADE) 
+    phone_number = models.CharField(max_length=15, blank=True, null=True) 
+    address = models.TextField(blank=True, null=True) 
+    
+    def __str__(self): 
+        return self.user.username
