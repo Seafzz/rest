2fc2d005f329c0ef67c2d983107b4d7c861ab596
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-12-04 01:05:45 +0000

    update: still checking for bugs

--- booking/views.py
+++ booking/views.py
@@ -49,12 +49,14 @@ def create_reservation(request):
             reservation.user = request.user
             reservation.save()
             print(f"Reservation created: {reservation}")  # Debugging output
+            print(f"Reservation details: Name - {reservation.name}, Time - {reservation.reservation_time}, Guests - {reservation.number_of_guests}, User - {reservation.user}")
             return redirect('my_reservations')
         else:
             print("Form is not valid")  # Debugging output
     else:
         form = ReservationForm()
     return render(request, 'booking/create_reservation.html', {'form': form})
+
 
 
 @login_required
